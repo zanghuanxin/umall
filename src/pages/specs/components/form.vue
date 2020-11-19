f805812ebdb199ee81ec413cf8040373b681f20f
zanghuanxin <1036814310@qq.com>
2020-11-19 18:02:59 +0800

    11/19-zhx-更改商品规格验证

--- src/pages/specs/components/form.vue
+++ src/pages/specs/components/form.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="add">
     <el-dialog :title="info.title" :visible.sync="info.isshow" @closed="closed">
-      <el-form :model="user" :rules="rules">
-        
+      <el-form :model="user" :rules="rules" ref="formName">
         <el-form-item label="规格名称" label-width="120px" prop="specsname">
           <el-input v-model="user.specsname" autocomplete="off"></el-input>
         </el-form-item>
 
-
-<el-form-item label="规格属性" label-width="120px" v-for="(item,index) in attrArr" :key="index" prop="attrs">
+        <el-form-item
+          label="规格属性"
+          label-width="120px"
+          v-for="(item,index) in attrArr"
+          :key="index"
+          prop="attrs"
+        >
           <div class="line">
             <el-input v-model="item.value" autocomplete="off" ></el-input>
-            <el-button type="primary" v-if="index===0" @click="addAttr" >添加规格属性</el-button>
+            <el-button type="primary" v-if="index===0" @click="addAttr">添加规格属性</el-button>
             <el-button type="danger" v-else @click="delAttr(index)">删除</el-button>
           </div>
         </el-form-item>
 
-        
         <el-form-item label="状态" label-width="120px">
           <el-switch v-model="user.status" :active-value="1" :inactive-value="2"></el-switch>
         </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="cancel">取 消</el-button>
+        <div class="footer">
+          <el-button @click="cancel">取 消</el-button>
         <el-button type="primary" v-if="info.title=='添加规格'" @click="add">添 加</el-button>
         <el-button type="primary" v-else @click="update">修 改</el-button>
-      </div>
-        </el-dialog>
+        </div>
+      </el-form>
+      
+    </el-dialog>
   </div>
 </template>
 
@@ -34,121 +38,122 @@
 import { errAlert, successAlert } from "../../../utils/alert";
 import path from "path";
 import { mapGetters, mapActions } from "vuex";
-import {  reqspecsAdd,
+import {
+  reqspecsAdd,
   reqspecsDetail,
   reqspecsUpdate,
-  reqspecsDel } from "../../../utils/http";
+  reqspecsDel,
+} from "../../../utils/http";
 export default {
   props: ["info"],
-    data() {
+  data() {
     return {
       user: {
         specsname: "",
         attrs: "",
-        status: 1
+        status: 1,
       },
-        attrArr: [{ value: "" }],
-         rules: {
-        specsname: [{ required: true, message: "请输入规格名称", trigger: "blur" }], 
-      
-        
+      attrArr: [{ value: "" }],
+      rules: {
+        specsname: [
+          { required: true, message: "请输入规格名称", trigger: "blur" },
+        ],
+    
       },
     };
   },
-  computed:{
-...mapGetters({
-   
-})
+  computed: {
+    ...mapGetters({}),
   },
   methods: {
     cancel() {
       this.info.isshow = false;
-      this.empty() 
+      this.empty();
     },
-     addAttr() {
+    addAttr() {
       this.attrArr.push({
-        value: ""
+        value: "",
       });
     },
-      delAttr(index) {
+    delAttr(index) {
       this.attrArr.splice(index, 1);
     },
     empty() {
-    this.user = {
-     specsname: "",
+      this.user = {
+        specsname: "",
         attrs: "",
-        status: 1
-      }
-       this.attrArr = [{ value: "" }];
+        status: 1,
+      };
+      this.attrArr = [{ value: "" }];
     },
-       check() {
-      return new Promise((resolve, reject) => {
-        //验证
-        if (this.user.specsname === "") {
-        errAlert("规格名称不能为空");
-          return;
-        }
-         
-          
-        
-        resolve();
-      });
-    },
- 
-  
-    add() {     
- this.check().then(() => {
+
+    add() {
+      this.$refs.formName.validate((valid) => {
         //添加逻辑
-          this.user.attrs = JSON.stringify(this.attrArr.map(item => item.value));
-      reqspecsAdd(this.user).then(res => {
-        if (res.data.code === 200) {
-          successAlert("添加成功");
-          this.cancel();
-          this.empty();
-          //刷新list
-          this.reqList()
-          this.reqCount()
+        if (valid) {
+          if(this.attrArr.some(item=>item.value==="")){
+            errAlert("请输入规格属性")
+            return;
+          }
+          this.user.attrs = JSON.stringify(
+            this.attrArr.map((item) => item.value)
+          );
+          reqspecsAdd(this.user).then((res) => {
+            if (res.data.code === 200) {
+              successAlert("添加成功");
+              this.cancel();
+              this.empty();
+              //刷新list
+              this.reqList();
+              this.reqCount();
+            } 
+          });
         }
+        return
       });
-      });
- 
     },
     ...mapActions({
-      reqList:"specs/reqList",
-        reqCount:"specs/reqCount",
+      reqList: "specs/reqList",
+      reqCount: "specs/reqCount",
     }),
     getOne(id) {
-      reqspecsDetail(id).then(res=>{
-            this.user=res.data.list[0]
-            this.attrArr=JSON.parse(this.user.attrs).map(item=>({value:item}))
-        })
-    },
-    update() {
-        this.check().then(() => {
-        //添加逻辑
-           this.user.attrs=JSON.stringify(this.attrArr.map(item=>item.value))
-        reqspecsUpdate(this.user).then(res=>{
-            if(res.data.code==200){
-                successAlert("更新成功")
-                this.cancel()
-                this.empty()
-                this.reqList()
-            }
-        })
+      reqspecsDetail(id).then((res) => {
+        this.user = res.data.list[0];
+        this.attrArr = JSON.parse(this.user.attrs).map((item) => ({
+          value: item,
+        }));
       });
     },
-    
-    
-    closed(){
-        if(this.info.title==='编辑规格'){
-            this.empty()
+    update() {
+      this.$refs.formName.validate((valid) => {
+        if (valid) {
+          this.user.attrs = JSON.stringify(
+            this.attrArr.map((item) => item.value)
+          );
+          reqspecsUpdate(this.user).then((res) => {
+            if (res.data.code == 200) {
+              successAlert("更新成功");
+              this.cancel();
+              this.empty();
+              this.reqList();
+            } else {
+              errAlert(res.data.msg);
+            }
+          });
+        } else {
+          errAlert("添加失败");
         }
-    }
-  },
+      });
+      //添加逻辑
+    },
 
-  mounted() {
-  
+    closed() {
+      if (this.info.title === "编辑规格") {
+        this.empty();
+      }
+    },
   },
+  mounted() {},
 };
 </script>
 
@@ -156,11 +161,21 @@ export default {
 .line {
   display: flex;
 }
+
 .line .el-input {
   flex: 1;
 }
+
 .line .el-button {
   width: auto;
 }
-
+.add{
+  position:relative;
+}
+.footer{
+    position:absolute;
+    bottom:10px;
+    right:10px;
+ 
+}
 </style>
